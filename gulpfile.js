3edbbf6be9bd54f5a746a66107e2620f7fb26722
Murillo de Morais <murillo.morais@vianuvem.com.br>
2016-09-13 15:01:14 -0300

    feat: added less and some functions automators

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,31 @@ var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var rename = require('gulp-rename');
+var less = require('gulp-less');
+var path = require('path');
+var cleanCSS = require('gulp-clean-css');
+var clear = require('clear');
 	
 gulp.task('clean-app', function () {
+	clear();
 	notify('Cleaning application folder');
     return del(['app']);
+});
+	
+gulp.task('clean-app-folder', function (folder) {
+	eval('clean_folder.' + folder);
+	notify('Removing files from application JS folder');
+    return del(['app/js/**']);
 });
 	
 gulp.task('clean-app-folder-js', function () {
 	notify('Removing files from application JS folder');
     return del(['app/js/**']);
+});
+	
+gulp.task('clean-app-folder-css', function () {
+	notify('Removing files from application CSS folder');
+    return del(['app/css/**']);
 });
 
 gulp.task('install-components', function(){
@@ -49,8 +65,21 @@ gulp.task('jade', function() {
         		.pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['lint','compress-js','jade'], function() {
+gulp.task('less', ['clean-app-folder-css'], function () {
+  return gulp.src('src/less/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['less','lint','compress-js','jade'], function() {
 	notify('Running a HTTP Server in the port 3000');
+	
 	browserSync.init({
         server: {
 			baseDir: "app"
@@ -60,7 +89,18 @@ gulp.task('serve', ['lint','compress-js','jade'], function() {
 	gulp.watch("src/js/*.js").on('change', function(){
 		gulp.start(['lint','compress-js']);
 	});
-	gulp.watch("src/*.jade").on('change', browserSync.reload);
+
+	gulp.watch("src/less/*.less").on('change', function(){	
+		gulp.start('less');
+	});
+
+	gulp.watch("src/*.jade").on('change', function(){
+		gulp.start('jade');
+	});
+
+	gulp.watch("app/**").on('change', function(){
+		browserSync.reload();
+	});
 });
 
 gulp.task('default', gulpsync.sync(['clean-app','install-components','serve'],'default'));
