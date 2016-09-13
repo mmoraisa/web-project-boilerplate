e5a3ab8b9790e37e89e97368b5a05e2fa6433117
Murillo de Morais <murillo.morais@vianuvem.com.br>
2016-09-13 08:59:58 -0300

    feat: added npm to control gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,35 @@
-console.log('ok');
+var gulp = require('gulp');
+var del = require('del');
+var install = require('gulp-install');
+var jade = require('gulp-jade');
+var browserSync = require('browser-sync').create();
+var gulpsync = require('gulp-sync')(gulp);
+
+gulp.task('clean-app', function () {
+    return del(['app']);
+});
+
+gulp.task('install-components', function(){
+	return gulp.src(['./bower.json'])
+  				.pipe(install());
+});
+
+gulp.task('jade', function() {
+    return gulp.src('./src/*.jade')
+    			.pipe(jade())
+    			.pipe(gulp.dest('./app/'));
+});
+
+gulp.task('serve', function() {
+
+	browserSync.init({
+        server: {
+			baseDir: "src"
+		}
+	});
+
+    gulp.watch("src/**").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', gulpsync.sync(['clean-app','install-components','jade','serve'],'default'));
